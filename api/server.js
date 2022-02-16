57b25b0637db1310c05ea16ce814d30aaef0d140
capriceebanks <capri.ee@outlook.com>
2022-02-16 08:49:34 +0000

    made changes to server.js

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const cors = require('cors');
+
+router.use(cors());
+router.use(express.json());
+
+// show route
+
+// index route
+
+// post route
+
+// create route
+
+// destroy route
+
